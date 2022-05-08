0134928af5cd963bbcf502578b670692af785bd3
MineClever <36216195+MineClever@users.noreply.github.com>
2022-05-08 15:35:04 +0800

    # add dependencies

--- scripts/template.bat
+++ scripts/template.bat
@@ -22,7 +22,7 @@ for /f "delims=" %%i in ('python %~dp0\gen_guid.py') do (
 set "jsonTab=    "
 set "rn=&echo."
 
-echo [!rn!%jsonTab%{!rn!%jsonTab%%jsonTab%"guid": "!guid!",!rn!%jsonTab%%jsonTab%//[POPULARS, GAMES, PROGRAMMING, SYSTEM, GENERIC, OTHERS]!rn!%jsonTab%%jsonTab%"category": "GAMES",!rn!%jsonTab%%jsonTab%"name": "TemplateName",!rn!%jsonTab%%jsonTab%"description": "None",!rn!%jsonTab%%jsonTab%"icon": "game.ico",!rn!%jsonTab%%jsonTab%"rules": [!rn!%jsonTab%%jsonTab%%jsonTab%"game.exe"!rn!%jsonTab%%jsonTab%]!rn!%jsonTab%}!rn!]|clip
+echo [!rn!%jsonTab%{!rn!%jsonTab%%jsonTab%"guid": "{7b8da2ff-59bf-479f-ba09-28b71bb6312e}",!rn!%jsonTab%%jsonTab%//[POPULARS, GAMES, PROGRAMMING, SYSTEM, GENERIC, OTHERS]!rn!%jsonTab%%jsonTab%"category": "GAMES",!rn!%jsonTab%%jsonTab%"name": "TemplateName",!rn!%jsonTab%%jsonTab%"description": "None",!rn!%jsonTab%%jsonTab%"icon": "game.ico",!rn!%jsonTab%%jsonTab%"rules": [!rn!%jsonTab%%jsonTab%%jsonTab%"game.exe"!rn!%jsonTab%%jsonTab%],!rn!%jsonTab%%jsonTab%//EAC="{9907e4d7-5ec6-408d-9fc7-adc96f3cd1a2}", Battleye="{e8cce56b-d127-478b-9e4c-ce9ec2a2f3f0}"!rn!%jsonTab%%jsonTab%"dependencies": [!rn!%jsonTab%%jsonTab%%jsonTab%""!rn!%jsonTab%%jsonTab%]!rn!%jsonTab%}!rn!]!rn!|clip
 goto :EOF
 
 :EOF
